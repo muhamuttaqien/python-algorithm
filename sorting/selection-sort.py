4b7c6de8643216a5157de52024d7e72706321fdd
Muh. Angga Muttaqien <angga.muttaqien@ruma.co.id>
2017-08-27 03:13:27 +0700

    add selection sort

---
+++ sorting/selection-sort.py
@@ -0,0 +1,25 @@
+
+def selection_sort(collection):
+
+    length = len(collection)
+    for i in range(length):
+        least = i
+
+        for k in range(i + 1, length):
+            if collection[k] < collection[least]:
+                least = k
+
+        collection[least], collection[i] = (
+            collection[i], collection[least]
+        )
+
+    return collection
+
+def main():
+    print("=== Selection Sort (Algorithm) - A sorting algorithm that has thime complexity, making it inefficient on large lists, and generally performs worse than the other similar sort")
+    numbers = raw_input("Enter numbers separated by a comma: ")
+    unsorted = [int(item) for item in numbers.split(',')]
+    print(selection_sort(unsorted))
+
+if __name__=='__main__':
+	main()
